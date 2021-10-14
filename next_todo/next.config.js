1efa64efdf8ce082254067bddce96ef6d8b5a4c0
oct-sky-out <kms3335k@naver.com>
2021-10-14 18:10:44 +0900

    scss적용, 리액트 아이콘 사용

--- next_todo/next.config.js
+++ next_todo/next.config.js
@@ -1,3 +1,12 @@
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
+  cssModules: true,
+});
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: { includePaths: [path.join(__dirname, 'styles')] },
 };
