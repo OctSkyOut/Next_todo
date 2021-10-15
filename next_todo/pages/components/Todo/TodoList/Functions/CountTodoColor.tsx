38f043720db733d14972a285dab427cc712e7ffb
oct-sky-out <kms3335k@naver.com>
2021-10-15 20:54:54 +0900

    todo_mock데이터제작, 타입제작, todo_list 일부 제작

---
+++ next_todo/pages/components/Todo/TodoList/Functions/CountTodoColor.tsx
@@ -0,0 +1,16 @@
+import { TodoType } from '../../../../../types/todo';
+import { useMemo } from 'react';
+
+type ColorIndexType = {
+  [key: string]: number;
+};
+export default function countTodoColors(todos: TodoType[]) {
+  return useMemo(() => {
+    const colors: ColorIndexType = {};
+    todos.forEach((todo) => {
+      colors[todo.color] =
+        colors[todo.color] !== undefined ? colors[todo.color] + 1 : 1;
+    });
+    return colors;
+  }, [todos]);
+}
